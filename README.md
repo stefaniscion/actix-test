8eacaa7c4f7df8aaed4be11330498796ff3d7d8c
Stefano Di Pasquale <stefaniscion@gmail.com>
2023-03-09 01:15:24 +0100

    first commit, added hello world

---
+++ README.md
@@ -0,0 +1,5 @@
+# Actix Tests
+Some my tests with actix-web to see how it works.
+
+## Summary
+- test_1: Simple "Hello World" example
---
+++ test_1/src/main.rs
@@ -0,0 +1,15 @@
+// Simple Hello World
+use actix_web::{web, App, HttpServer, Responder};
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
